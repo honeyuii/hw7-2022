595dff35814356e24aa80b844dfd9d402f0a368d
honeyuii <112532769+honeyuii@users.noreply.github.com>
2023-04-02 18:47:34 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,67 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	video.loop = false;
+	
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	document.querySelector('#volume').innerHTML = video.volume * 100 + '%';
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down");
+	video.playbackRate -= 0.1;
+	console.log("New speed: " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate += 0.1;
+	console.log("New speed: " + video.playbackRate);
+})
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 10 < video.duration) {
+		video.currentTime += 10;
+	} else {
+		video.currentTime = 0;
+	}
+	console.log("Current time: " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted) {
+		video.muted = false;
+		document.querySelector('#mute').innerHTML = 'Mute';
+	} else {
+		video.muted = true;
+		document.querySelector('#mute').innerHTML = 'Unmute';
+	}
+});
+
+document.querySelector("#slider").addEventListener("input", function() {
+
+	video.volume = this.value / 100;
+	updateVolume();
+	document.getElementById("volume").innerHTML = (video.volume*100)+"%";
+	console.log(video.volume);
+});
+
+ocument.querySelector('#vintage').addEventListener('click', function(){
+	video.classList.add('oldSchool');
+})
+
+document.querySelector('#orig').addEventListener('click', function(){
+	video.classList.remove('oldSchool');
+})
